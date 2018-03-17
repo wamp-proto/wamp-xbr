0200c6e98c574ce24468dc3d08ebfc064dec6884
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-03-17 09:26:12 +0100

    started with xbr network/market contracts

---
+++ main/contracts/XBRNetwork.sol
@@ -0,0 +1,72 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2018 Crossbar.io Technologies GmbH and contributors.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+pragma solidity ^0.4.0;
+
+contract XBRNetwork {
+    address public network_token;
+    address public network_sponsor;
+    address public dns_oracle;
+
+    struct Domain {
+        uint256 cookie;
+    }
+    mapping(string => Domain) _domains;
+
+    struct DomainVerification {
+        uint started;
+        Domain domain;
+    }
+    mapping(uint256 => DomainVerification) _domain_verifications;
+
+    function XBRNetwork(address network_token, address network_sponsor, address dns_oracle) public {
+        network_token = network_token;
+        network_sponsor = network_sponsor;
+        dns_oracle = dns_oracle;
+    }
+
+    /**
+     *
+     */
+    function claim_domain(string domain, string descriptor) returns (uint256) {
+        // FIXME
+    }
+
+    function verify_domain(uint256 domain_cookie, uint8 v, bytes32 r, bytes32 s) {
+        // FIXME
+    }
+
+    // Register an XBR agent in a data market.
+    //
+    // @public_key: The WAMP-cryptosign (Ed25519) public key of the peer
+    // @block_number: The Ethereum block number from which onwards the association
+    //                should be established.
+    // @descriptor: The IPFS object content address of the agent descriptor bundle.
+    //              e.g. `QmarHSr9aSNaPSR6G9KFPbuLV9aEqJfTk1y9B8pdwqK4Rq`.
+    //
+    function register_agent(bytes32 public_key, uint block_number, string descriptor) {
+        // FIXME
+    }
+
+    function is_signed_by(address signer, bytes32 hash, uint8 v, bytes32 r, bytes32 s) constant returns (bool) {
+
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHash = keccak256(prefix, hash);
+        return ecrecover(prefixedHash, v, r, s) == signer;
+    }
+}
