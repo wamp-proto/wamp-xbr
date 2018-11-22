986ae2af6d9576f3a8c78c181396481f9fdf90d8
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-11-22 17:47:00 +0100

    sync

--- .solcover.js
+++ .solcover.js
@@ -4,5 +4,8 @@ module.exports = {
     norpc: false,
     port: 9545,
     testCommand: "truffle test --network coverage",
-    testrpcOptions: '--port 9545 --gasLimit 0xfffffffffff --gasPrice 1',
+    testrpcOptions: '--port 9545 --networkId 5777 --gasLimit 0xfffffffffff --gasPrice 1 --accounts 15 --defaultBalanceEther 100000000 --deterministic --mnemonic "myth like bonus scare over problem client lizard pioneer submit female collect"',
+    dir: '.',
+    copyPackages: ['openzeppelin-solidity'],
+    skipFiles: ['Migrations.sol','XBRMaintained.sol', 'XBRNetwork.sol', 'XBRNetworkProxy.sol', 'XBRPaymentChannel.sol']
 };
