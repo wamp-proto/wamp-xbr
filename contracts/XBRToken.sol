1e932111e6fc373197f456a045673766a3fe862b
Tobias Oberstein <tobias.oberstein@crossbario.com>
2019-09-06 17:22:54 +0200

    sync

--- contracts/XBRToken.sol
+++ contracts/XBRToken.sol
@@ -18,6 +18,7 @@
 
 pragma solidity ^0.5.2;
 
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
