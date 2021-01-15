b41aca4d4eca8f487ff7d8a53bf257d55c19da02
Tobias Oberstein <tobias.oberstein@crossbario.com>
2021-01-15 03:45:41 +0100

    sync

--- contracts/XBRMaintained.sol
+++ contracts/XBRMaintained.sol
@@ -18,7 +18,7 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-pragma solidity ^0.6.0;
+pragma solidity >=0.6.0 <0.8.0;
 pragma experimental ABIEncoderV2;
 
 // import "openzeppelin-solidity/contracts/access/AccessControl.sol";
